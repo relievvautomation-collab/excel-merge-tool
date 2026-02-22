7d90b1e7947421a4cea60a44f4beb98df3c34489
Relievv Automation <relievvautomation@gmail.com>
2026-02-23 05:15:44 +0530

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,10 +1,6 @@
-import multiprocessing
-
-bind = "0.0.0.0:10000"
-workers = multiprocessing.cpu_count() * 2 + 1
-threads = 4
 timeout = 600
-worker_class = "gthread"
-max_requests = 1000
-max_requests_jitter = 50
-preload_app = True
+graceful_timeout = 600
+keepalive = 5
+worker_class = "sync"
+workers = 1
+threads = 4
