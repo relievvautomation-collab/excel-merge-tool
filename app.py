b9080e65a79d82ced9de5853ae085a1b638ebd0b
relievvautomation-collab <relievvautomation@gmail.com>
2026-02-22 18:59:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -935,4 +935,8 @@ if __name__ == '__main__':
     except:
         pass
     
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    if __name__ == '__main__':
+        import os
+        port = int(os.environ.get("PORT", 10000))
+        app.run(host='0.0.0.0', port=port)
+
